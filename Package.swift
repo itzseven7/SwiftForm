0fe684947ba0135a45790b4fb68721e40ae19839
Romain <>
2019-10-31 12:47:48 +0100

    update spm package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,5 +21,6 @@ let package = Package(
             name: "SwiftFormTests",
             dependencies: ["SwiftForm"],
             path: "SwiftFormTests"),
-    ]
+    ],
+    swiftLanguageVersions: [.v4_2, .v5]
 )
